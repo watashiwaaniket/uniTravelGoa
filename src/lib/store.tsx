d58b6beb37cc2282b24e8699d72d5fac9b9c5d7e
Aniket Dhakane <aniketdhakane3@gmail.com>
2025-02-26 20:59:24 +0530

    finished the south vs goa page

---
+++ src/lib/store.tsx
@@ -0,0 +1,59 @@
+export const NorthGoaData: string = `
+
+- **Fort Aguada -** The fort was constructed in 1612 is a well-preserved seventeenth-century Portuguese fort standing in Goa. Aguada is a Portuguese word which means “Watering Place” and the fort was built by the then Portuguese rulers for providing fresh water supply to the ships passing by in the Arabian Sea.
+    
+- **Aguada Light House** - One of the distinctive features of the Aguada headlands are the Lighthouses built as early as 1864 thereby marking them as one of the oldest and first lighthouses to be built in Asia.
+    
+- **Sinquerium Fort --** The Sinquerim Fort is located at a distance of 18 kms. from Panaji. The fort stands overlooking the Sinquerim beach and bisects the shoreline into two. This fort was constructed in the year 1612. The is an extension of the larger upper Aguada fort. The lower fort consists of a bastion and a section of fortified wall on both sides. During the 16th and 17th Century ships used to anchor next to the fort.
+    
+- **Vagator Beach -** Vagator beach is one of the most scenic beaches in Goa that is situated immediately north of the Anjuna beach overlooked by steep laterite hills. The crescent shaped beach has become a favorite spot among tourists for midnight parties. The beach is also well known as the 500 Old Portuguese fort of Chapora is situated nearby.
+    
+- **Anjuna Beach** - The beach is famous for its stunning natural beauty with swaying palms trees and soft powered white sands. The beach is marked by an unusual rocky formation overlying a cove of white sand and black rock that juts out into the Sea.
+    
+- **Baga Beach** - One of the favourite beach in bardez side and a tourist attraction. Goa&apos;s Baga beach is a small but perfect beach located between its two more famous cousins, Anjuna on one side and Calangute on the other. It is a good place to enjoy some quite moments and dwell in peaceful contemplation with oneself.
+    
+- **Snow Park** -Indoor park with a sledding hill & ice bar, plus areas for kids & adults to play in manmade snow.
+    
+    **Trip Timing 10:00 am to 06:00 pm**
+`
+
+export const SouthGoaData: string = `
+- **Dona Paula Beach**- An estuary overlooking the port city of Goa, Vasco-da-Gama and simply known as the lover paradise.
+    
+- **Dolphin Trip**-Dolphin spotting trip is very popular on your visit to Goa. You hear lot about dolphins in Arabian sea. one hour of cruising in Arabian sea spotting dolphin is very pleasing trip. These extremely intelligent and playful mammals will delight every visitor with their antics as they play and feed in the wild. Apart from dolphin spotting the trip will also take you around Goa&apos;s.''Old jail, Fort Agauda, light house and famous millionaire palace''.
+    
+- **The Se cathedral de Santa Catarina**, known as Se Cathedral, is the cathedral of the Latin Rite Roman Catholic Archdiocese of Goa and Daman and the seat of the Patriarch of the East Indies. It is located in **Old Goa.**
+    
+- · Panjim Church **/** White chruch -Visit where you would find the mortal remains of the patron Saint of Goa, St. Francis Xavier.
+    
+- **Tirupati Balaji Temple-** This Private temple complex belonging to GSB community. 33 km from Panaji at the foothill of Kavalem village in Ponda Taluka.
+    
+- **Mangueshi Temple** - A temple dedicated to Lord Shiva and the birthplace of Bharat Ratna awardee and renowned singer Lata Mangeshkar.
+    
+- **Miramar Beach** - Located barely 03 kms from the Capital, this beach welcomes you to swaying Pine trees and also holds the mortal remains of the first Chief Minister of Goa.
+    
+- **Sunset River Cruise** - Cruise down the River Mandovi. Enjoy the traditional Goan & Portuguese dances performed on board and dance to the tune of some scintillating music.
+    
+    **Trip Timing 10:00 am to 06:00 pm**
+`
+
+export const TourExcluded: string = `
+
+ Any charges for video cameras, still cameras etc.
+    
+ Soft drinks, beverages, Personal Expenses etc.
+    
+ Snow Park Ticket
+    
+ Night Boat Cruise
+    
+ Dolphin Trip Extra Charges
+
+ Pick and drop for this tour is available for hotels in following areas: 
+ 
+ Calangute, Baga, Arpora, Nagoa, Candolim, Sinquerim, & Nerul only
+
+ if your hotel is not on the main road, then you will have to walk up to the main road for the pick-up 
+
+ if For areas other than the above-mentioned areas, transport requests will be charged extra.
+`
--- src/pages/SightSeeing.tsx
+++ src/pages/SightSeeing.tsx
@@ -1,19 +1,27 @@
 import Navbar from "../components/Navbar"
 import southNorthGoa from "/south-north-goa.jpg"
+import Markdown from "react-markdown"
+import { NorthGoaData, SouthGoaData, TourExcluded } from "../lib/store.js"
 
 export default function SightSeeing() {
     return(
         <>
             <Navbar />
-            <div className="h-screen w-screen flex flex-col justify-center items-center bg-[#B5828C] bg-grid-white">
-                <img src={southNorthGoa} alt="south vs north goa" />
-                <div className="flex w-[680px] h-auto">
-                    <div className="w-[340px] bg-slate-500">
-
+            <div className="h-auto min-h-screen w-screen flex flex-col justify-center items-center bg-[#B5828C] bg-grid-white">
+                <img src={southNorthGoa} alt="south vs north goa" className="mt-28"/>
+                <div className="flex w-[900px] h-auto font-semibold bg-black bg-opacity-75">
+                    <div className="w-[450px] pt-4 p-2">
+                        <h2 className="text-2xl font-bold pb-6"># North Goa Tour Data</h2>
+                        <Markdown>{NorthGoaData}</Markdown>
                     </div>
-                    <div className="w-[340px] bg-slate-800">
-
+                    <div className="w-[450px] pt-4 p-2">
+                        <h2 className="text-2xl font-bold pb-6"># South Goa Tour Data</h2>
+                        <Markdown>{SouthGoaData}</Markdown>
                     </div>
+                </div>
+                <div className="flex flex-col w-[900px] h-auto font-semibold bg-black bg-opacity-75 items-center justify-center pb-8">
+                    <h2 className="text-2xl font-bold py-6">What is Not included in the tour!</h2>
+                    <p className="p-4 flex flex-col justify-center items-center"><Markdown>{TourExcluded}</Markdown></p>
                 </div>
             </div>
         </>
